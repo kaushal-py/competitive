afa7362a4251fadef26d275ffb607d80a3afb038
kb-studios <2015kaushal.bhogale@ves.ac.in>
2019-02-17 15:58:39 +0530

    Nov challenge 2018

---
+++ Nov18/CHFTIRED.py
@@ -0,0 +1,7 @@
+n = int(input())
+
+for _ in range(n):
+
+    a,b = map(int, input().split())
+
+    print("YES")
---
+++ Nov18/PRDRG.py
@@ -0,0 +1,5 @@
+in_arr = [int(x) for x in input().split()]
+
+for i in range(1, len(in_arr)):
+    print(1, 2**in_arr[i], end=' ')
+print()
