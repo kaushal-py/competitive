7ae1fb65f2a3069e4d9724adce5d4540b8ebb9bb
kb-studios <2015kaushal.bhogale@ves.ac.in>
2019-02-23 19:22:41 +0530

    basic template for contests

---
+++ template.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+int main(){
+
+    // solve_nvt()
+    // solve()
+    // solve_t()
+    
+}
+
+void solve(){
+
+
+}
+
+void solve_t(){
+    
+    int t;
+    cin >> t;
+
+    while(t--){
+
+    }
+}
+
+void solve_nvt(){
+
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int i,j,k,n;
+        cin >> n;
+        int n;
+
+        vector<int> v;
+
+        for(int i=0; i<n; i++){
+            int temp;
+            cin >> temp;
+            v.push_back(temp);
+        }
+
+    }
+}
